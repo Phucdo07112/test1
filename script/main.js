ab31ece118eb09a79013db082b4b8f86230c143d
Phucdo07112 <85002157+Phucdo07112@users.noreply.github.com>
2022-06-01 15:39:44 +0700

    update
    update

--- script/main.js
+++ script/main.js
@@ -1,17 +1,10 @@
-const toggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-const menuItem = document.querySelectorAll('.menu-item');
+const toggle = $('.menu-toggle');
+const menu = $('.menu');
+const menuItem = $('.menu-item');
 const activeClass = "is-show";
-toggle.addEventListener('click',function() {
-  menu.classList.add(activeClass);
+toggle.click(function() {
+  menu.addClass(activeClass);
+});
+menuItem.click(function() {
+  menu.removeClass(activeClass)
 })
-menuItem.forEach((key, value) => {
-    key.addEventListener('click',function() {
-        menu.classList.remove(activeClass);
-    })
-})
-// window.addEventListener('click', function(e){   
-//   if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")){
-//     menu.classList.remove(activeClass);
-//   } 
-// });
